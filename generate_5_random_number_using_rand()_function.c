863027a211a5432cda8601ab1c61290e80313756
Metehan GENCER <112508775+engineerMeteHan@users.noreply.github.com>
2023-09-29 14:14:29 +0300

    Add files via upload

---
+++ generate_5_random_number_using_rand()_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    // It returns the same sequence of random number on every execution of the program
+
+    printf("Random numbers are:\n");
+    for(int i = 0; i < 5; i++)
+        printf("%d\n", rand());
+
+    return 0;
+}
+
+/*
+    As we can see in the above output, it returns the s
+    ame sequence of random numbers on every execution
+    of the oragramming code
+**/
+
+/// Metehsn GENCER
