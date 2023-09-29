258404a010163cc968c681d4b3b7b312579a9022
Metehan GENCER <112508775+engineerMeteHan@users.noreply.github.com>
2023-09-29 15:14:25 +0300

    with_randon_number_function

--- generate_random_numbers_using_srand().c
+++ generate_random_numbers_using_srand().c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>     // use time.h header file to use time
 
+void random_number(int random);
+
 int main()
 {
     int num, i;
@@ -17,4 +19,21 @@ int main()
     // generate random number between 0 to 50
     for (i = 0; i < num; i++)
         printf("%d. %d\n", i+1, rand() % 50);
+
+         printf("Enter a number to set the limit for random number\n");
+     scanf("%d", &randon_num);
+
+     random_number(randon_num);
+}
+
+void random_number(int random)
+{
+    printf("randon_nember() function called\n");
+    time_t t1;
+    srand((unsigned)time(&t1));
+
+    // generate random number between 0 to 100
+    for(int i = 0; i < random; i++)
+        printf("%d. %d\n", i + 1, rand() % 100);
+
 }
